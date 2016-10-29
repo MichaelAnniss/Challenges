05a0d18c549cc2b61e9104f2952c567467747cc3
Michael <mja2g16@soton.ac.uk>
2016-10-29 16:41:34 +0100

    [Week4] Allow scripts to be run given through command line

--- src/main/java/me/mikey/challenges/week4/Week4.java
+++ src/main/java/me/mikey/challenges/week4/Week4.java
@@ -2,6 +2,7 @@ package me.mikey.challenges.week4;
 
 import me.mikey.challenges.week4.interpreter.BareBones;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
@@ -12,12 +13,35 @@ import java.util.Scanner;
 public class Week4 {
     public static void main(String[] args) {
         //warm up vm for more realistic benchmarks
-        run("multiply.bb", true);
-        run("multiply.bb", true);
-        run("multiply.bb", false);
+        runResource("multiply.bb", true);
+        runResource("multiply.bb", true);
+
+        if(args.length == 0) {
+            runResource("multiply.bb", false);
+        } else {
+            run(args[0], false);
+        }
     }
 
     public static void run(String fileName, boolean silent) {
+        try {
+            File file = new File(fileName);
+
+            Scanner scanner = new Scanner(file);
+            StringBuilder code = new StringBuilder();
+
+            while (scanner.hasNext())
+                code.append(scanner.nextLine()).append("\n");
+
+            BareBones.evaluate(code.toString(), silent);
+
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runResource(String fileName, boolean silent) {
         try(InputStream is = Week4.class.getClassLoader().getResourceAsStream(fileName)) {
             Scanner scanner = new Scanner(is);
             StringBuilder code = new StringBuilder();
@@ -26,7 +50,6 @@ public class Week4 {
                 code.append(scanner.nextLine()).append("\n");
 
             BareBones.evaluate(code.toString(), silent);
-
             scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
